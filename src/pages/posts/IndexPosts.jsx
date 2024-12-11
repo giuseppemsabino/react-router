f366d59445a7738dece5d65a2a5315335f9a715b
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-11 18:08:28 +0100

    data map in table

--- src/pages/posts/IndexPosts.jsx
+++ src/pages/posts/IndexPosts.jsx
@@ -1,43 +1,74 @@
-export default function IndexPosts() {
+import { useEffect, useState } from "react";
 
-    const apiUrl = import.meta.env.VITE_API_URL
-    // console.log(apiUrl);
-    
+export default function IndexPosts() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  //   console.log(apiUrl);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    fetch(apiUrl + "/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        
+        console.log(
+          data.map((post) => ({
+            id: post.id,
+            image: post.image,
+            title: post.title,
+            category: post.category,
+            published: post.published,
+          }))
+        );
+      });
+  };
 
   return (
     <div className="container">
       <h1 className="mt-5">Lista dei post</h1>
 
       <table className="table table-dark table-hover">
-      <thead>
-    <tr>
-    <th scope="col">#</th>
-            <th scope="col">Titolo</th>
+        <thead>
+          <tr>
             <th scope="col">Immagine</th>
+            <th scope="col">Titolo</th>
             <th scope="col">Categoria</th>
             <th scope="col">Pubblicato</th>
-            <th scope="col">Azioni</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+            <th scope="col">Preview</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">immagine</th>
+            <td>post article</td>
+            <td>food</td>
+            <td>false</td>
+            <td>
+              <a href="#">Show</a>-<a href="#">Edit</a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">immagine</th>
+            <td>post article</td>
+            <td>food</td>
+            <td>false</td>
+            <td>
+              <a href="#">Show</a>-<a href="#">Edit</a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">immagine</th>
+            <td>post article</td>
+            <td>food</td>
+            <td>false</td>
+            <td>
+              <a href="#">Show</a>-<a href="#">Edit</a>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
